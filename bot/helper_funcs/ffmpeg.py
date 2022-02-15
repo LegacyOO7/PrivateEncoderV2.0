67d74524f19008f9aaa3ac4aec4b0368a98c7b7e
legion <88172317+LegacyOO7@users.noreply.github.com>
2022-02-15 07:16:27 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -41,7 +41,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
      ## lol 😂
     crf.append("32")
     watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=25:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=AnimeSerisLoverz"')
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]} -c:v libx265 -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s 854x480 -b:v 150k -c:a libopus -b:a 40k "{out_put_file_name}" -y'
+    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]} -c:v libx265 -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s 854x480 -b:v 150k -metadata title="AnimeSeriesLoverz™ | 480p"  -metadata:s:v title="AnimeSeriesLoverz - 480p"  -metadata:s:a title="Opus ~ AnimeSeriesLoverz" -metadata:s:s title="English ~ AnimeSeriesLoverz" -c:a libopus -b:a 40k "{out_put_file_name}" -y'
  #Done !!
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
